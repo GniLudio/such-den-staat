51409e3ef5540741f182501682f7e28def7cddef
GniLudio <ferdinand.rohlfing@googlemail.com>
2025-04-09 16:18:44 +0200

    Add text to the settings dialog

--- src/components/SettingsDialog.vue
+++ src/components/SettingsDialog.vue
@@ -2,6 +2,11 @@
     <v-container>
         <v-card>
             <v-card-title class="text-center">Einstellungen</v-card-title>
+            <v-card-text class="text-center">
+                Noch keine Einstellungen verfügbar.
+                <br />
+                Kommen aber pünktlich wie die ...
+            </v-card-text>
         </v-card>
     </v-container>
 </template>
