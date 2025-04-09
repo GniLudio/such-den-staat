2cd7d75a997f7c88873e3735cd8c8a02268030ad
GniLudio <ferdinand.rohlfing@googlemail.com>
2025-04-09 15:51:21 +0200

    Fix marker icon

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -7,9 +7,17 @@
     import "leaflet/dist/leaflet.css";
     import "leaflet-boundary-canvas";
     import * as L from "leaflet";
+    import markerIconUrl from "leaflet/dist/images/marker-icon.png";
+    import markerIconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
+    import markerShadowUrl from "leaflet/dist/images/marker-shadow.png";
 
     import { onMounted } from "vue";
     import { useLeafletStore } from "@/stores/leafletStore";
+
+    L.Icon.Default.prototype.options.iconUrl = markerIconUrl;
+    L.Icon.Default.prototype.options.iconRetinaUrl = markerIconRetinaUrl;
+    L.Icon.Default.prototype.options.shadowUrl = markerShadowUrl;
+    L.Icon.Default.imagePath = "";
 
     // Options
     const mapOptions: L.MapOptions = {
