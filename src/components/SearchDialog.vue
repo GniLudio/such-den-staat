5f25529c5aba81465453676066ad39088d2a4f89
GniLudio <ferdinand.rohlfing@googlemail.com>
2025-04-17 21:35:12 +0200

    Use array for all tab info in SearchDialog.vue

--- src/components/SearchDialog.vue
+++ src/components/SearchDialog.vue
@@ -2,36 +2,48 @@
     <v-card class="justify-content px-3 pb-1">
         <div class="d-flex">
             <v-tabs v-model="tab" align-tabs="center" class="w-100 mb-2" center-active>
-                <v-tab value="autobahnen">Autobahnen</v-tab>
+                <v-tab :value="t.id" v-for="t in tabs">{{ t.label }}</v-tab>
             </v-tabs>
             <SearchButton class="my-auto" v-on:search="search" :loading="loading"></SearchButton>
         </div>
         <v-tabs-window v-model="tab" class="mb-1">
-            <v-tabs-window-item value="autobahnen">
-                <AutobahnTab ref="autobahnTab"></AutobahnTab>
+            <v-tabs-window-item :value="t.id" v-for="t in tabs">
+                <component :is="t.type" :ref="t.ref"></component>
             </v-tabs-window-item>
         </v-tabs-window>
         <v-progress-linear :model-value="loadingProgress" :class="{ 'opacity-0': !loading }"></v-progress-linear>
     </v-card>
 </template>
 <script setup lang="ts">
-    import { computed, ref, watch, type ComputedRef, type Ref } from "vue";
+    import { computed, ref, watch, type Component, type ComputedRef, type Ref } from "vue";
+    import AutobahnTab from "./searchTabs/AutobahnTab.vue";
 
-    const tab: Ref<TabID> = ref("autobahnen");
-    const autobahnTab: Ref<TabComponent | undefined> = ref();
-    const tabs: Record<TabID, Ref<TabComponent | undefined>> = {
-        "autobahnen": autobahnTab,
-    }
-    const tabInstance: Ref<Ref<TabComponent | undefined> | undefined> = ref();
+    const tabs: Tab[] = [
+        {
+            id: "autobahnen",
+            label: "Autobahnen",
+            type: AutobahnTab,
+            ref: ref(),
+        },
+    ];
+    const tab: Ref<TabID> = ref(tabs[0].id);
 
-    const search: ComputedRef<Function | undefined> = computed(() => tabInstance.value?.value?.search);
-    const loading: ComputedRef<boolean> = computed(() => Object.values(tabs).map((t) => t.value?.loading).some((l) => l));
-    const loadingProgress: ComputedRef<number> = computed(() => Math.max(...Object.values(tabs).map((t) => t.value?.loading ? t.value.loadingProgress : 0)));
+    const tabRefs: ComputedRef<(TabComponent | undefined)[]> = computed(() => tabs.map((t) => t.ref.value));
+    const tabRef: ComputedRef<TabComponent | undefined> = computed(() => tabs.find((t) => t.id == tab.value)?.ref.value);
 
-    watch(tab, (newValue) => {
-        tabInstance.value = tabs[newValue];
+    const search: ComputedRef<Function | undefined> = computed(() => tabRef.value?.search);
+    const loading: ComputedRef<boolean> = computed(() => tabs.map((t) => t.ref.value?.loading).some((l) => l));
+    const loadingProgress: ComputedRef<number> = computed(() => {
+        return Math.max(...tabRefs.value.map((ref) => ref?.loading ? ref.loadingProgress : 0));
     });
 
+    // Types
+    interface Tab {
+        id: TabID,
+        label: string,
+        type: Component<TabComponent>,
+        ref: Ref<TabComponent | undefined>,
+    }
     interface TabComponent {
         search: () => void | Promise<void>,
         loading: boolean,
