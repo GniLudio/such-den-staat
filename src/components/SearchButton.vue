ea05ef01074ba311d69c065cb227274032282a24
GniLudio <ferdinand.rohlfing@gmail.com>
2025-04-19 12:31:19 +0200

    Remove props binding from search button

--- src/components/SearchButton.vue
+++ src/components/SearchButton.vue
@@ -1,9 +1,8 @@
 <template>
-    <v-btn color="primary" type="submit" v-bind="props" @click="emit('search')">
+    <v-btn color="primary" type="submit" @click="emit('search')">
         <v-icon icon="mdi-magnify"></v-icon>
     </v-btn>
 </template>
 <script setup lang="ts">
-    const props = defineProps();
     const emit = defineEmits(["search"]);
 </script>
